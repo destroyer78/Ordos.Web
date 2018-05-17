f4a599c228729aae3b25a9bc3273175b4b86ba38
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-05-17 19:09:43 -0400

    Fix scroll offset for features

--- Ordos.Web/wwwroot/js/site.js
+++ Ordos.Web/wwwroot/js/site.js
@@ -7,9 +7,16 @@
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
-                $('html, body').animate({
-                    scrollTop: (target.offset().top - 48)
-                }, 1000, "easeInOutExpo");
+                if (target[0].id == "features") {
+                    $('html, body').animate({
+                        scrollTop: (target.offset().top + 80)
+                    }, 1000, "easeInOutExpo");
+                }
+                else {
+                    $('html, body').animate({
+                        scrollTop: (target.offset().top - 48)
+                    }, 1000, "easeInOutExpo");
+                }
                 return false;
             }
         }
