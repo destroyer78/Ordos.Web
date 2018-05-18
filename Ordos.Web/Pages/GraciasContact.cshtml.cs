24aeffda42c41f5a79375a9ef0714eefa4be4f06
Gabriel De La Parra <ignacio.delaparra@gmail.com>
2018-05-18 12:41:15 -0400

    fix cdn jquery-val

---
+++ Ordos.Web/Pages/GraciasContact.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ordos.Web.Pages
+{
+    public class GraciasContactModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
